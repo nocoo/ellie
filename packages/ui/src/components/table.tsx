76970e017fc7bff54a91c559861786883836325c
Zheng Li <lizheng@lizheng.me>
2026-05-13 19:27:20 +0800

    style(ui): replace bg-muted with bg-background in table component per B05 spec

--- packages/ui/src/components/table.tsx
+++ packages/ui/src/components/table.tsx
@@ -34,7 +34,7 @@ function TableFooter({ className, ...props }: React.ComponentProps<"tfoot">) {
 	return (
 		<tfoot
 			data-slot="table-footer"
-			className={cn("border-t bg-muted/50 font-medium [&>tr]:last:border-b-0", className)}
+			className={cn("border-t bg-background/50 font-medium [&>tr]:last:border-b-0", className)}
 			{...props}
 		/>
 	);
@@ -45,7 +45,7 @@ function TableRow({ className, ...props }: React.ComponentProps<"tr">) {
 		<tr
 			data-slot="table-row"
 			className={cn(
-				"border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+				"border-b transition-colors hover:bg-background/50 data-[state=selected]:bg-background",
 				className,
 			)}
 			{...props}
